b4d2aaa826e37e088fdc76a85a330d6fbf04f82d
DavitJincharadze <113067688+J1NCH0@users.noreply.github.com>
2023-03-01 19:04:02 +0400

    gavagrdzelebmale

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+function gotoInstagram () {
+    window.location.href='https://www.instagram.com/dato_jincharadze12/'
+}
+
+function gotoFacebook () {
+    window.location.href='https://www.facebook.com/dato.jinchaadze'
+}
+
+function gotoTwitter () {
+    window.location.href='https://twitter.com/DavitJincho'
+}
+
+function gotoPinterest () {
+    window.location.href='https://pin.it/1an3Ivg'
+}
